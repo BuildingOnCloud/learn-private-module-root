cee131cb293f27a012e761513abccad35e5e8757
BuildingOnCloud <72888433+BuildingOnCloud@users.noreply.github.com>
2021-04-13 07:23:33 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
-  description = "This is the cloud hosting region where your webapp will be deployed."
+  description = "us-east-1"
 }
 
 variable "prefix" {
-  description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
+  description = "dev"
 }
 
 variable "name" {
-  description = "Your name to attach to the webapp address"
+  description = "StaticWeSite"
 }
